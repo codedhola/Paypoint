5b7550a002a4d969a11532196c247e76f8faf3f4
coded hola <codedhola2000@gmail.com>
2022-12-01 17:27:10 +0100

    updated the client structure

--- client/app.js
+++ client/app.js
@@ -1,19 +1,35 @@
-const feetch = document.querySelector("#fetch").addEventListener("click", function(e){
-    console.log("clicked")
-    fetch("https://billing-staging.bytestacks.io/api/v1/bundles", {
-		method: 'POST',
-		headers: {
-            "Content-type" : "application/json",
-            "api-key": "Fincra_6VA35CDPQNMRS"
-		},
-        body: JSON.stringify({
-            "telco": "AIRTEL"
-        })
-	})
-    .then(data => data.json())
-    .then(data => console.log(data))
-})
+// const feetch = document.querySelector("#fetch").addEventListener("click", function(e){
+//     console.log("clicked")
+//     fetch("https://billing-staging.bytestacks.io/api/v1/bundles", {
+// 		method: 'POST',
+// 		headers: {
+//             "Content-type" : "application/json",
+//             "api-key": "Fincra_6VA35CDPQNMRS"
+// 		},
+//         body: JSON.stringify({
+//             "telco": "AIRTEL"
+//         })
+// 	})
+//     .then(data => data.json())
+//     .then(data => console.log(data))
+// })
 
+fetch("https://billing-staging.bytestacks.io/api/v1/electricity/billers", {
+    method: "GET",
+    headers: {
+        "Content-type" : "application/json",
+        "api-key": "Fincra_6VA35CDPQNMRS"
+    }
+})
+    .then(data => data.json())
+    .then(telco => {
+        console.log(telco)
+        telco.data.map(li => {
+        let list = document.createElement("li")
+        list.innerHTML = li.name
+        document.getElementById("plan-list").append(list)
+       })
+    })
 
 
 
