8baad4f39efb90386714f75c24fdcacb6f75ef1f
coded hola <codedhola2000@gmail.com>
2022-12-04 01:51:39 +0100

    refactor: moved endpoint call to server-side for security purpose

--- server/server.js
+++ server/server.js
@@ -1,11 +1,33 @@
 const express = require("express")
 const cors = require("cors")
+const axios = require("axios")
 const app = express()
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false}))
 
-app.get("/", (req, res) => {
+app.get("/",  (req, res) => {
+    // axios.get("https://billing-staging.bytestacks.io/api/v1/electricity/billers",{}, {
+    //     headers: {
+    //         "Content-type" : "application/json",
+    //         "api-key": "Fincra_6VA35CDPQNMRS"
+    //     }
+    // })
+
+    const options = {
+        method: 'GET',
+        url: 'https://billing-staging.bytestacks.io/api/v1/electricity/billers',
+        headers: {accept: 'application/json', 'content-type': 'application/json', "api-key": "Fincra_6VA35CDPQNMRS"}
+      };
+      
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
     res.status(200).json({
         status: "success",
         message: "loading project..."
