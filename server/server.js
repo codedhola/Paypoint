1ea435e695eda66225749de815f8402cbb271370
coded hola <codedhola2000@gmail.com>
2022-12-12 10:18:50 +0100

    refactor: for Fincra article logic

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.urlencoded({ extended: false}))
 
-app.get("/",  async (req, res) => {
+app.get("/", (req, res) => {
     const body = req.body;
     res.status(200).json({
         status: "success",
